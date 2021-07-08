64a21c729a408491274541c0726df7c183a47591
Sankacku <sankacku@gmail.com>
2021-07-08 23:20:07 +0200

    workflow test with flask test 3

--- flask_test.py
+++ flask_test.py
@@ -1,9 +1,28 @@
 from flask import Flask
-# import time
-# import lgpio
-# relay_pin_1 = 24
-# h = lgpio.gpiochip_open(0)
-# lgpio.gpio_claim_output(h,relay_pin_1)
+import time
+import lgpio
+
+
+relay_pin_1 = 2
+relay_pin_2 = 3
+relay_pin_3 = 4
+relay_pin_4 = 14
+relay_pin_5 = 15
+relay_pin_6 = 18
+
+
+pin_nr1 = lgpio.gpiochip_open(0)
+pin_nr2 = lgpio.gpiochip_open(0)
+pin_nr3 = lgpio.gpiochip_open(0)
+pin_nr4 = lgpio.gpiochip_open(0)
+pin_nr5 = lgpio.gpiochip_open(0)
+pin_nr6 = lgpio.gpiochip_open(0)
+lgpio.gpio_claim_output(pin_nr1,relay_pin_1)
+lgpio.gpio_claim_output(pin_nr2,relay_pin_2)
+lgpio.gpio_claim_output(pin_nr3,relay_pin_3)
+lgpio.gpio_claim_output(pin_nr4,relay_pin_4)
+lgpio.gpio_claim_output(pin_nr5,relay_pin_5)
+lgpio.gpio_claim_output(pin_nr6,relay_pin_6)
 
 app = Flask(__name__)
 
@@ -12,14 +31,40 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/<name>')
-def hello_world2(name):
+@app.route('/<number>')
+def hello_world2(number):
 #     # lgpio.gpio_write(h,relay_pin_1, 1)
 #     # time.sleep(20)
 #     # lgpio.gpiochip_close(h)
 #     # print("Still working")
-    print(name)
-    return f'Hello {name}! <h1> Unlocking 1 </h1>'
+    print("Going to activate Number: " + number + " for 3 Seconds now")
+    if int(number) == 1:
+        lgpio.gpio_write(pin_nr1,relay_pin_1, 0)
+        time.sleep(3)
+        lgpio.gpio_write(pin_nr1,relay_pin_1, 1)
+    elif int(number) == 1:
+        lgpio.gpio_write(pin_nr2,relay_pin_2, 0)
+        time.sleep(3)
+        lgpio.gpio_write(pin_nr2,relay_pin_2, 1)
+    elif int(number) == 1:
+        lgpio.gpio_write(pin_nr3,relay_pin_3, 0)
+        time.sleep(3)
+        lgpio.gpio_write(pin_nr3,relay_pin_3, 1)
+    elif int(number) == 1:
+        lgpio.gpio_write(pin_nr4,relay_pin_4, 0)
+        time.sleep(3)
+        lgpio.gpio_write(pin_nr4,relay_pin_4, 1)
+    elif int(number) == 1:
+        lgpio.gpio_write(pin_nr5,relay_pin_5, 0)
+        time.sleep(3)
+        lgpio.gpio_write(pin_nr5,relay_pin_5, 1)
+    elif int(number) == 1:
+        lgpio.gpio_write(pin_nr6,relay_pin_6, 0)
+        time.sleep(3)
+        lgpio.gpio_write(pin_nr6,relay_pin_6, 1)
+    else:
+        print("noob")
+    return f'<h1> Hello !! </h1> <h1> Unlocking {number} </h1>'
      
 
 if __name__ == "__main__":
