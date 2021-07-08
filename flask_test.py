674c50e7af5b26d0d13df328d1d117b1e6aaa619
Sankacku <sankacku@gmail.com>
2021-07-08 18:49:35 +0200

    workflow test with flask test 3

--- flask_test.py
+++ flask_test.py
@@ -12,14 +12,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/test')
-def hello_world2():
-     print("Working here")
+@app.route('/<name>')
+def hello_world2(name):
 #     # lgpio.gpio_write(h,relay_pin_1, 1)
 #     # time.sleep(20)
 #     # lgpio.gpiochip_close(h)
 #     # print("Still working")
-#     return 'Hello World! <h1> Unlocking 1 </h1>'
+     return 'Hello {name}! <h1> Unlocking 1 </h1>'
 
 
 # if __name__ == "__main__":
