a19fda4dfb6c01e29c3afcf365e4539cca09e60d
Sankacku <sankacku@gmail.com>
2021-07-08 12:41:08 +0200

    workflow test with flask test 1

--- flask_test.py
+++ flask_test.py
@@ -17,24 +17,24 @@ def hello_world():
 @app.route('/test/')
 def hello_world():
     try:
-    print("Working here")
-    # x = 0.1
-    # while x<8:
-    #     # Turn the GPIO pin on
-    #     lgpio.gpio_write(h,relay_pin_1, 1)
-    #     time.sleep(x)
+        print("Working here")
+        # x = 0.1
+        # while x<8:
+        #     # Turn the GPIO pin on
+        #     lgpio.gpio_write(h,relay_pin_1, 1)
+        #     time.sleep(x)
 
-    #     # Turn the GPIO pin off
-    #     lgpio.gpio_write(h,relay_pin_1, 0)
-    #     time.sleep(x)
-    #     x = x + 1
-    lgpio.gpio_write(h,relay_pin_1, 1)
-    time.sleep(20)
-    lgpio.gpiochip_close(h)
-    print("Still working")
-except KeyboardInterrupt:
-    print("Not working here")
-return 'Hello World! <h1> Unlocking 1 </h1>'
+        #     # Turn the GPIO pin off
+        #     lgpio.gpio_write(h,relay_pin_1, 0)
+        #     time.sleep(x)
+        #     x = x + 1
+        lgpio.gpio_write(h,relay_pin_1, 1)
+        time.sleep(20)
+        lgpio.gpiochip_close(h)
+        print("Still working")
+    except KeyboardInterrupt:
+        print("Not working here")
+    return 'Hello World! <h1> Unlocking 1 </h1>'
 
 
 if __name__ == "__main__":
